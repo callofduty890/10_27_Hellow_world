33c53572b9382b6a97b2364c595aa25b67c3a4d0
俊华 蔡 <1032031912@qq.com>
2020-10-27 09:27:23 +0800

    添加项目文件。

---
+++ 10_27_创建项目/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//控件名称
+namespace _10_27_创建项目
+{
+    class Program//类
+    {
+        //主入口函数
+        static void Main(string[] args)
+        {
+            //向控制台输出信息
+            Console.WriteLine("你好 世界!");
+            Console.ReadKey();//等待控制体的输出
+        }
+    }
+}
